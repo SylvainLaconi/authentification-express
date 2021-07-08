694bb076e55e3914355d0a4b76ff802ca2b12991
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-07-08 09:55:57 +0200

    .gitignore est maintenant fonctionnel

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
+const connection = require("./conf");
 const app = express();
 const port = process.env.PORT || 4040;
-const pool = require('./conf');
-const morgan = require('morgan');
-const cors = require('cors');
+const pool = require("./conf");
+const morgan = require("morgan");
+const cors = require("cors");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,20 +15,17 @@ app.use(cors());
 // app.use('/', homeRouter)
 // app.use('/courses', coursesRouter)
 
-
 pool.getConnection((err) => {
-   if (err) {
-    console.error('error connecting to db', err
-  );
-} else {
-  console.log('connected to db');
-}
+  if (err) {
+    console.error("error connecting to db", err);
+  } else {
+    console.log("connected to db");
+  }
 });
 
-
 app.listen(port, (err) => {
-  if(err){
-    throw new Error('Something had happened', err)
+  if (err) {
+    throw new Error("Something had happened", err);
   }
-  console.log(`server is listening on port ${port}`)
+  console.log(`server is listening on port ${port}`);
 });
