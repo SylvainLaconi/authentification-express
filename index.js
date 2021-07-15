b85f0eb5e1df92763d1de5f0b2fe130ff13dc9af
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-07-15 16:40:07 +0200

    routes register & login ok

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const connection = require("./conf");
+const db = require("./conf");
 const app = express();
 const port = process.env.PORT || 4040;
 const pool = require("./conf");
 const morgan = require("morgan");
 const cors = require("cors");
+const authRouter = require("./routes/auth");
+const loginRouter = require("./routes/login");
+const dbName = process.env.DB_NAME;
 
 app.use(morgan("dev"));
 
@@ -12,16 +15,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-// app.use('/', homeRouter)
-// app.use('/courses', coursesRouter)
-
 pool.getConnection((err) => {
   if (err) {
-    console.error("error connecting to db", err);
+    console.error(`error connecting to ${dbName}`, err);
   } else {
-    console.log("connected to db");
+    console.log(`connected to ${dbName}`);
   }
 });
+
+app.use("/register", authRouter);
+app.use("/login", loginRouter);
 
 app.listen(port, (err) => {
   if (err) {
---
+++ routes/login.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/hash");
+const db = require("../conf");
+const checkAuthFields = require("../middlewares/check-fields");
+const checkJwt = require("../middlewares/check-jwt");
+const router = express.Router();
+const isProduction = process.env.NODE_ENV === "production";
+require("dotenv").config();
+
+router.post("/", checkAuthFields, (req, res) => {
+  const { email, password } = req.body;
+
+  db.query("SELECT * FROM user WHERE email = ? ", email, (err, results) => {
+    const [user] = results;
+
+    User.verifyPassword(password, user.password, err).then(
+      (passwordIsCorrect) => {
+        if (err) {
+          console.log(err.message);
+        } else if (!passwordIsCorrect) {
+          res.status(400).json({
+            errors: "invalid password",
+          });
+        } else {
+          const { id } = user;
+          const payload = { id, email };
+          const privateKey = process.env.JWT_SECRET;
+
+          jwt.sign({ payload }, privateKey, (jwterr, token) => {
+            if (jwterr) {
+              return res.status(500).json({
+                errors: [jwterr.message],
+              });
+            }
+            const options = {
+              httpOnly: true,
+              expiresIn: "1h",
+              secure: isProduction,
+            };
+            res.cookie("jwt", token, options);
+            res.json({ payload });
+          });
+        }
+      }
+    );
+  });
+});
+
+router.get("/check", checkJwt, (req, res) => {
+  res.json(req.user);
+});
+
+module.exports = router;
